996984721ccf7210bc1aa68ced7088d644a6b01e
qi <1445461016@qq.com>
2021-07-28 18:34:24 +0800

    second commit

--- src/main/java/com/zq/test/GitTest01.java
+++ src/main/java/com/zq/test/GitTest01.java
@@ -3,5 +3,6 @@ package com.zq.test;
 public class GitTest01 {
     public static void main(String[] args){
         System.out.println("hello Git!");
+        System.out.println("hello git!");
     }
 }
