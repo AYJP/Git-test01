46c644c1db1a828082f24d9c3d1926ba7d600ab4
qi <1445461016@qq.com>
2021-07-29 15:57:04 +0800

    push test

--- src/main/java/com/zq/test/GitTest01.java
+++ src/main/java/com/zq/test/GitTest01.java
@@ -6,5 +6,6 @@ public class GitTest01 {
         System.out.println("hello git!");
         System.out.println("Hello git!");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
